dd872f76e01222e39b8f5180bc7ebdb596124e26
FahimFBA <fahimbinamin@gmail.com>
2022-07-07 12:33:16 +0600

    add : source code

---
+++ 7_July_2022/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    x = 5; // assigned the value; = is getting used as the assignment operator
+    // assignment operator
+    if (x % 2 == 1) // 5 % 2 = 1 == 1
+        cout << "It is an odd number" << endl;
+    // = assignment operator
+    // == comparison operator
+}
